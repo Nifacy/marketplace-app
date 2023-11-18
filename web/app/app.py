2fae48a0a5391ae15efcb62887eb7f0656586af0
Eldi <enderel2812@gmail.com>
2023-11-18 20:42:55 +0300

    fix

--- web/app/app.py
+++ web/app/app.py
@@ -18,16 +18,6 @@ async def shutdown():
     global conn
     conn.close()
 
-# TODO: check if can be changed to be used, and remove if not
-# @app.post("/suppliers/", response_model=Supplier)
-# async def create_supplier_endpoint(supplier: Supplier):
-#     try:
-#         create_supplier(conn, supplier)
-#     except Exception as e:
-#         raise HTTPException(status_code=400, detail=str(e))
-
-#     return supplier
-
 
 @app.get("/suppliers/{supplier_id}", response_model=Supplier)
 async def get_supplier_endpoint(supplier_id: int):
