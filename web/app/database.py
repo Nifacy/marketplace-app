133327dab79f1290f50d8a7f033e7e55363de081
Alexey Grishin <Alexey.Grishin@kaspersky.com>
2023-11-23 13:41:55 +0300

    added type hints in db script

--- web/app/database.py
+++ web/app/database.py
@@ -1,16 +1,17 @@
 import psycopg2
+from psycopg2.extensions import connection
+
 from .config import settings
 
 
-def _init_db(conn):
+def _init_db(conn: connection) -> None:
     cur = conn.cursor()
     with open('sql/initialise.sql', 'r') as f:
         cur.execute(f.read())
     conn.commit()
 
 
-def connect():
+def connect() -> connection:
     conn = psycopg2.connect(str(settings.database_url))
     _init_db(conn)
     return conn
-
