20a39cc307da7c013f05f4f16f67c51a65d3db4c
Nifacy <mai.grishin.alexey@gmail.com>
2023-11-25 01:10:40 +0300

    fix: updated supplier usecases

--- web/app/usecases/supplier.py
+++ web/app/usecases/supplier.py
@@ -14,11 +14,11 @@ def get_supplier(conn: psycopg2.extensions.connection, supplier_id: int) -> Supp
     # 0 - id; 1 - name; 2 - contacts; 3 - address
     supplier_data = cur.fetchone()
 
-    contacts = get_contacts(conn, supplier_data[2])
-    address = get_address(conn, supplier_data[3])
-
     if supplier_data is None:
         raise SupplierNotFound()
+
+    contacts = get_contacts(conn, supplier_data[2])
+    address = get_address(conn, supplier_data[3])
     
     supplier_info = SupplierInfo(
         name=supplier_data[1],
@@ -35,14 +35,14 @@ def get_supplier(conn: psycopg2.extensions.connection, supplier_id: int) -> Supp
     return supplier
 
 
-def create_supplier_info(conn: psycopg2.extensions.connection, supplier_info: SupplierInfo) -> None:
+def create_supplier(conn: psycopg2.extensions.connection, supplier_info: SupplierInfo) -> int:
     cur = conn.cursor()
 
     address_id = create_address(conn, supplier_info.address)
     contacts_id = create_contacts(conn, supplier_info.contacts)
 
     cur.callproc(
-        'create_supplier_info', 
+        'create_supplier', 
         (
             supplier_info.name, 
             contacts_id, 
@@ -50,6 +50,10 @@ def create_supplier_info(conn: psycopg2.extensions.connection, supplier_info: Su
         )
     )
 
+    supplier_id = cur.fetchone()
     cur.close()
 
-    # Продолжение неуверенности в supplier_id.
+    if supplier_id is None:
+        raise UnableToCreateSupplier()
+
+    return supplier_id[0]
