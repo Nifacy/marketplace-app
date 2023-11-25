97b52ef8f9829e0a68b52662bfbe22ae8ff37b20
Nifacy <mai.grishin.alexey@gmail.com>
2023-11-25 16:31:40 +0300

    moved customer info creation in utils

--- web/tests/usecases/test_customer.py
+++ web/tests/usecases/test_customer.py
@@ -1,45 +1,18 @@
 import pytest
 
-from app.schemas import CustomerInfo, Address, Contacts
 from app.usecases import customer
 
-
-_counter = 0
-
-
-def create_customer_sample() -> CustomerInfo:
-    global _counter
-    _counter += 1
-
-    return CustomerInfo(
-        first_name=f'customer-{_counter}',
-        last_name='last-name',
-        contacts=Contacts(
-            phone='+1 (123) 456-7890',
-            email='test.email@mail.com',
-            telegram='@testsupplier',
-        ),
-        address=Address(
-            street='Street',
-            city='Moscow',
-            country='Russia',
-            postal_code='12345',
-            house=1,
-            entrance=1,
-            appartment=1,
-        )
-    )
-
+from . import utils
 
 def test_customer_creation(db_connection):
-    expected_customer_info = create_customer_sample()
+    expected_customer_info = utils.create_customer_info_sample()
     created_customer = customer.create_customer(db_connection, expected_customer_info)
     
     assert created_customer.info == expected_customer_info
 
 
 def test_customer_getable_after_creation(db_connection):
-    expected_customer_info = create_customer_sample()
+    expected_customer_info = utils.create_customer_info_sample()
     created_customer = customer.create_customer(db_connection, expected_customer_info)
     found_customer = customer.get_customer(db_connection, created_customer.id)
 
--- web/tests/usecases/utils.py
+++ web/tests/usecases/utils.py
@@ -1,13 +1,19 @@
 import itertools
+from typing import Callable, TypeVar
+from app.schemas import Address, Contacts, SupplierInfo, CustomerInfo
 
-from app.schemas import Address, Contacts, SupplierInfo
+_T = TypeVar('_T')
 
+# TODO: make type hints better here
+def with_counter(func: Callable[..., _T]) -> Callable[..., _T]:
+    _counter = itertools.count(start=1)
+    def wrapper(*args, **kwargs) -> _T:
+        count = next(_counter)
+        return func(count, *args, **kwargs)
+    return wrapper
 
-_counter = itertools.count(start=1)
-
-def create_supplier_info_sample() -> SupplierInfo:
-    count = next(_counter)
-
+@with_counter
+def create_supplier_info_sample(count: int) -> SupplierInfo:
     return SupplierInfo(
         name=f'test-supplier-{count}',
         contacts=Contacts(
@@ -26,4 +32,23 @@ def create_supplier_info_sample() -> SupplierInfo:
         )
     )
 
-
+@with_counter
+def create_customer_info_sample(count: int) -> CustomerInfo:
+    return CustomerInfo(
+        first_name=f'customer-{count}',
+        last_name='last-name',
+        contacts=Contacts(
+            phone='+1 (123) 456-7890',
+            email='test.email@mail.com',
+            telegram='@testsupplier',
+        ),
+        address=Address(
+            street='Street',
+            city='Moscow',
+            country='Russia',
+            postal_code='12345',
+            house=1,
+            entrance=1,
+            appartment=1,
+        )
+    )
