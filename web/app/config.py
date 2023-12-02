a546068e4d64fcc504e8d3ec97086af472524918
Eldi <enderel2812@gmail.com>
2023-12-02 23:42:17 +0300

    got rid of TODO: comment

--- web/app/config.py
+++ web/app/config.py
@@ -11,7 +11,6 @@ class DatabaseConnectionType(Enum):
 class Settings(BaseSettings):
     database_url: PostgresDsn
     db_connection_type: DatabaseConnectionType = Field(alias='DATABASE_CONNECTION_TYPE')
-    # TODO: uncomment after auth realization
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
