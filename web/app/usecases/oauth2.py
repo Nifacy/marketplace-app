2c0af18234486d956a6c569bb1b8fa23732eb4e9
Eldi <enderel2812@gmail.com>
2023-12-03 23:51:40 +0300

    OAUTH2, TEST_OAUTH2 tests passed

--- web/app/usecases/oauth2.py
+++ web/app/usecases/oauth2.py
@@ -1,15 +1,24 @@
 from typing import Literal
+from fastapi import status, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from pydantic import ValidationError
+from psycopg2.extensions import connection
+
 from app.config import settings
-from app.schemas import TokenData
+from app.schemas import Customer, Supplier, TokenData
+from .customer import get_customer
+from .supplier import get_supplier
 from ._exceptions import *
 
 
 SECRET_KEY = settings.secret_key
 ALGORITHM = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 
 def generate_token(data: TokenData) -> str:
@@ -20,7 +29,7 @@ def generate_token(data: TokenData) -> str:
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-def decode_token(token: str) -> TokenData:
+def verify_access_token(token: str, credentials_exception) -> TokenData:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
 
@@ -33,6 +42,28 @@ def decode_token(token: str) -> TokenData:
         return TokenData(type=token_type, id=int(id))
     
     except JWTError:
-        raise UnableDecodeToken("Unable to decode token")
+        raise credentials_exception
     except ValidationError:
         raise UnableDecodeToken("Invalid token data")
+
+
+def get_current_user(conn: connection, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Could not validate credentials", 
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+
+    token_data = verify_access_token(token, credentials_exception)
+
+    try:
+        if token_data.type == 'supplier':
+            user = get_supplier(conn, token_data.id)
+        elif token_data.type == 'customer':
+            user = get_customer(conn, token_data.id)
+        else:
+            raise HTTPException(status_code=400, detail="Invalid user type")
+    except (SupplierNotFound, CustomerNotFound):
+        raise credentials_exception
+
+    return user
--- web/tests/usecases/test_oauth2.py
+++ web/tests/usecases/test_oauth2.py
@@ -1,11 +1,76 @@
+from fastapi import status, HTTPException
+from pydantic import ValidationError
 import pytest
-from app.schemas import TokenData
-from app.usecases.oauth2 import generate_token, decode_token
+
+from app.schemas import TokenData, Supplier, Customer
+from app.usecases.oauth2 import generate_token, verify_access_token, get_current_user
+from app.usecases import customer, supplier
+
+from . import utils
+
 
 def test_token_encoding_and_decoding():
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Could not validate credentials", 
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    
     data = TokenData(type='supplier', id=123)
 
     token = generate_token(data)
-    decoded_data = decode_token(token)
+    decoded_data = verify_access_token(token, credentials_exception)
 
     assert decoded_data == data
+
+
+def test_get_current_user_supplier(db_connection):
+    supplier_info = utils.create_supplier_info_sample()
+    created_supplier = supplier.create_supplier(db_connection, supplier_info)
+    token_data = TokenData(type='supplier', id=created_supplier.id)
+    token = generate_token(token_data)
+
+    user = get_current_user(db_connection, token)
+
+    assert isinstance(user, Supplier)
+    assert user.id == created_supplier.id
+    # TODO: Add more assertions to check the other attributes of the user if needed (don't think so)
+    # idk, in case of data corruption???
+
+
+def test_get_current_user_customer(db_connection):
+    customer_info = utils.create_customer_info_sample()
+    created_customer = customer.create_customer(db_connection, customer_info)
+    token_data = TokenData(type='customer', id=created_customer.id)
+    token = generate_token(token_data)
+
+    user = get_current_user(db_connection, token)
+
+    assert isinstance(user, Customer)
+    assert user.id == created_customer.id
+    # TODO: Add more assertions to check the other attributes of the user if needed (don't think so)
+    # idk, in case of data corruption???
+
+
+def test_get_current_user_invalid_token_data(db_connection):
+    token_data_dict = {"type": "invalid_type", "id": 99999}
+    with pytest.raises(ValidationError):
+        token_data = TokenData(**token_data_dict)
+        _ = generate_token(token_data)
+
+
+def test_get_current_user_random_string(db_connection):
+    token = "random_string"
+
+    with pytest.raises(HTTPException) as exc_info:
+        get_current_user(db_connection, token)
+    assert exc_info.value.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+def test_get_current_user_non_existent_user(db_connection):
+    token_data = TokenData(type='supplier', id=99999)
+    token = generate_token(token_data)
+
+    with pytest.raises(HTTPException) as exc_info:
+        get_current_user(db_connection, token)
+    assert exc_info.value.status_code == status.HTTP_401_UNAUTHORIZED
