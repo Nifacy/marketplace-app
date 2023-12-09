867566d6b41487c45efc0a11f00c80c115c01371
Nifacy <mai.grishin.alexey@gmail.com>
2023-12-09 10:28:47 +0300

    changed representation of auth dependency

---
+++ web/app/dependencies/__init__.py
@@ -0,0 +1,8 @@
+from .auth import get_current_user
+from . import database
+
+
+__all__ = [
+    "get_current_user",
+    "database",
+]
--- web/app/dependencies/auth.py
+++ web/app/dependencies/auth.py
@@ -4,28 +4,28 @@ from fastapi.security import OAuth2PasswordBearer
 
 from app import schemas
 from app.usecases import customer, oauth2, supplier
+from . import database
 
 
+_auth_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-class AccountAuthenticator:
-    __auth_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
-    def __init__(self, conn: psycopg2.extensions.connection):
-        self._connection = conn
+def get_current_user(
+    conn: psycopg2.extensions.connection = Depends(database.get_connection),
+    token: oauth2.Token = Depends(_auth_scheme),
+) -> schemas.Supplier | schemas.Customer:
+    try:
+        token_data = oauth2.decode_token(token)
 
-    def __call__(self, token: str = Depends(__auth_scheme)) -> schemas.Supplier | schemas.Customer:
-        try:
-            token_data = oauth2.decode_token(token)
+        if token_data.type == "supplier":
+            return supplier.get_supplier(conn, token_data.id)
 
-            if token_data.type == "supplier":
-                return supplier.get_supplier(self._connection, token_data.id)
+        else:
+            return customer.get_customer(conn, token_data.id)
 
-            else:
-                return customer.get_customer(self._connection, token_data.id)
-
-        except:
-            raise HTTPException(
-                status_code=401,
-                detail="Invalid token",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
+    except:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
