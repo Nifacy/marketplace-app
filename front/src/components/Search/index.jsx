5d12ceea8169023413562b59828479b4e0de955b
Kristian Nikulin <kristiannikulin@mail.ru>
2023-12-09 16:59:15 +0300

    2

--- front/src/components/Search/index.jsx
+++ front/src/components/Search/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Input } from "../Input";
 
-export const Search = () => {
+export const Search = (props) => {
+  const { placeholder } = props;
   //   const [searchName, setSearchName] = useState("");
-  return <Input />;
+  return <Input placeholder={placeholder} />;
 };
--- front/src/pages/Client/index.jsx
+++ front/src/pages/Client/index.jsx
@@ -3,19 +3,30 @@ import styles from "./styles.module.css";
 
 // import { useNavigate } from "react-router-dom";
 import { Nav } from "../../components/Nav";
-import { Search } from "../../components/Search";
 import { CardItem } from "../../components/CardItem";
+import { Input } from "../../components/Input";
 
 export const Client = () => {
   //   const navigate = useNavigate();
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([
+    {
+      url: "https://gas-kvas.com/uploads/posts/2023-02/1675489758_gas-kvas-com-p-izobrazheniya-i-kartinki-na-fonovii-risuno-41.jpg",
+      name: "qweqweqwe",
+      price: 123,
+    },
+  ]);
   const [isLoading, setIsLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleOnChangeSearch = (event) => {
+    setSearchValue(event.target.value);
+  };
 
   useEffect(() => {
     async function fetchData() {
       try {
         setIsLoading(true);
-        const items = await fetch(`http://localhost:3000/ALLitems`).then((res) => res.json());
+        const items = await fetch(`http://localhost:3000/ALLitems`).then((res) => res.json()); // id, name, price
         setItems(items);
       } catch (error) {
         console.log(error);
@@ -27,25 +38,26 @@ export const Client = () => {
   }, []);
 
   const renderItems = () => {
-    return (isLoading ? [...Array(8)] : items) //items.filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase()))
-      .map((item, index) => {
-        return (
-          <CardItem
-            {...item}
-            key={index}
-            // onClickPlus={(obj) => onAddToCart(obj)}
-            // onClickFavorite={(obj) => onAddToFavorite(obj)}
-            isLoading={isLoading}
-          />
-        );
-      });
+    return (
+      isLoading ? [...Array(8)] : items.filter((item) => item.name.toLowerCase().includes(searchValue.toLowerCase()))
+    ).map((item, index) => {
+      return (
+        <CardItem
+          {...item}
+          key={index}
+          // onClickPlus={(obj) => onAddToCart(obj)}
+          // onClickFavorite={(obj) => onAddToFavorite(obj)}
+          isLoading={isLoading}
+        />
+      );
+    });
   };
 
   return (
     <div className={styles.container}>
       <Nav />
       <div className={styles.main}>
-        <Search />
+        <Input placeholder="Поиск" value={searchValue} onChange={handleOnChangeSearch} />
         <div className={styles.items}>{renderItems()}</div>
       </div>
     </div>
